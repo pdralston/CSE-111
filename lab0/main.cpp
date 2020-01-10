029ccd4f877304112d8b4f535d291fcbde7e6086
Perry David Ralston Jr <ralstontech@gmail.com>
2020-01-10 13:12:17 -0800

    Fixed found issues in the Makefile (#3)
    * Fixed found issues in the Makefile
    
    Found some issues in the makefile and fixed them. THis needs to be tested still. I will create a branch to set up the basic files for compilation.
    
    * Created Basic compilable files.
    
    Tested makefile. working like a champ.

---
+++ lab0/main.cpp
@@ -0,0 +1,17 @@
+/**
+* Main.cpp
+* Main application file for wordcount
+* lab0.
+* @author Perry David Ralston Jr
+* @author Sasank Madineni
+* @date   Winter Quarter 2020
+* @class  CSE111 Advanced Programming
+*/
+
+#include <stdio.h>
+
+#include "wordlist.hpp"
+
+int main(int argc, char** argv) {
+  return 0;
+}
---
+++ lab0/wordlist.cpp
@@ -0,0 +1,12 @@
+/**
+* wordlist.hpp
+* Source file for wordlist. Wordlist
+* implements a list sorted lexicographically
+* with each element containing a word and a count.
+* @author Perry David Ralston Jr
+* @author Sasank Madineni
+* @date   Winter Quarter 2020
+* @class  CSE111 Advanced Programming
+*/
+
+#include "wordlist.hpp"
