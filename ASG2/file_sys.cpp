ee73006a37ca0bc343dcac9072e046201a1904aa
Perry David Ralston Jr <ralstontech@gmail.com>
2020-02-02 12:21:56 -0800

    removed extra space for prompt

--- ASG2/file_sys.cpp
+++ ASG2/file_sys.cpp
@@ -49,7 +49,7 @@ inode_state::inode_state() {
 const string& inode_state::prompt() const { return prompt_; }
 
 
-void inode_state::prompt(const string& prompt) { prompt_ = prompt + " "; }
+void inode_state::prompt(const string& prompt) { prompt_ = prompt; }
 
 
 void inode_state::make(wordvec& pathname, wordvec& data, bool relToRoot = false){
